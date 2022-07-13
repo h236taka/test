c6c42d3d18fea418e07c1a3ef614f8153f69315c
h236taka <108774142+h236taka@users.noreply.github.com>
2022-07-13 10:58:00 +0900

    Create a.php

---
+++ a.php
@@ -0,0 +1,11 @@
+#!/usr/bin/php
+<?php
+  foreach(file('/usr/share/dict/linux.words') as $line){
+    // strpos() 関数を使って、文字列 "abc" が含まれる単語を echo "$line"; で表示しなさい。
+    $find = 'abc';
+    $pos = strpos($line, $find);
+    if ( $pos == true ){           //strposがtrueならば$findを満たす$lineを返す
+      echo "$line";
+    }
+  }
+?>
