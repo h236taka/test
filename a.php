e4a58d5686426acc0ef2bfb4ac9fdaf34ba63b42
h236taka <108774142+h236taka@users.noreply.github.com>
2022-07-13 11:12:49 +0900

    Update a.php

--- a.php
+++ a.php
@@ -4,7 +4,7 @@
     // strpos() 関数を使って、文字列 "abc" が含まれる単語を echo "$line"; で表示しなさい。
     $find = 'abc';
     $pos = strpos($line, $find);
-    if ( $pos == true ){           //strposがtrueならば$findを満たす$lineを返す
+    if ( $pos !== false ){           //strposがtrueならば$findを満たす$lineを返す
       echo "$line";
     }
   }
